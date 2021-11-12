4ab938141870c1c105bdeccdf8beed07d54880e9
Schab <schabibyd@gmail.com>
2021-11-12 12:52:32 -0600

    nuevo
    ya se vio el cambio

--- sketch.js
+++ sketch.js
@@ -5,7 +5,7 @@ const Bodies = Matter.Bodies;
 var engine, world;
 var box1, pig1;
 var backgroundImg;
-var platform
+//var platform
 
 function preload() {
     backgroundImg = loadImage("sprites/bg.png");
@@ -33,7 +33,7 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-    platform=new Suelo(870,120,100, -PI/7)
+    //platform=new Suelo(870,120,100, -PI/7)
 
     bird = new Bird(100,100);
 
@@ -59,6 +59,6 @@ function draw(){
     box5.display();
     log4.display();
     log5.display();
-platform.display();
+//platform.display();
     bird.display();
 }
